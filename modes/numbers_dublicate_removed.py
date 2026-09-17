fd07ade2b8f200372a54c57e93525bb3b93400d6
bhimaTester <bhimatestingteam@gmail.com>
2026-09-17 15:54:08 +0530

    Auto update EDP Analytics - 2026-09-17 15:54:08

--- modes/numbers_dublicate_removed.py
+++ modes/numbers_dublicate_removed.py
@@ -1,7 +1,7 @@
 from core.config import LEGACY_DIR
 from core.loader import execute_legacy_script
 
-LEGACY_SOURCE = LEGACY_DIR / "CustomNumbersDublicateRemoved.py"
+LEGACY_SOURCE = LEGACY_DIR / "Custom-NumbersDublicateRemoved.py"
 
 
 def render() -> None:
