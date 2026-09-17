78faadb8707ee2758eacff6f5b7f942a74efb258
bhimaTester <bhimatestingteam@gmail.com>
2026-09-17 18:11:13 +0530

    Auto update EDP Analytics - 2026-09-17 18:11:13

--- modes/Scheme_Joining_Cashfree_Payment_Details_Reconciliation.py
+++
@@ -1,8 +0,0 @@
-from core.config import LEGACY_DIR
-from core.loader import execute_legacy_script
-
-LEGACY_SOURCE = LEGACY_DIR / "ERP-SchemeJoiningCashfreePaymentDetailsReconciliation.py"
-
-
-def render() -> None:
-    execute_legacy_script(LEGACY_SOURCE)
