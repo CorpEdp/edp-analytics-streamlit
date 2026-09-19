7cfb8304a4c0fe4bcd8ae1496d157cf11b0443ea
bhimaTester <bhimatestingteam@gmail.com>
2026-09-19 17:45:36 +0530

    Auto update EDP Analytics - 2026-09-19 17:45:36

--- modes/employee_referral_Performance_Report.py
+++ modes/employee_referral_Performance_Report.py
@@ -1,7 +1,7 @@
 from core.config import LEGACY_DIR
 from core.loader import execute_legacy_script
 
-LEGACY_SOURCE = LEGACY_DIR / "App-GatewayCashfreePaymentModeAnalytics.py"
+LEGACY_SOURCE = LEGACY_DIR / "App-Employee_referral_Performance_Report.py"
 
 
 def render() -> None:
