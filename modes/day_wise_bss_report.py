f77d44b14c19e30842412305fb5f04a8bba5d6e5
bhimaTester <bhimatestingteam@gmail.com>
2026-09-21 18:03:20 +0530

    Auto update EDP Analytics - 2026-09-21 18:03:20

--- modes/day_wise_bss_report.py
+++ modes/day_wise_bss_report.py
@@ -1,7 +1,7 @@
 from core.config import LEGACY_DIR
 from core.loader import execute_legacy_script
 
-LEGACY_SOURCE = LEGACY_DIR / "ERP-BssDaywiseReport.py"
+LEGACY_SOURCE = LEGACY_DIR / "ERP-Bss_Day_wise_Report.py"
 
 
 def render() -> None:
